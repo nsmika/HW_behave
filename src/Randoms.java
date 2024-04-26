c0ca0efa17ba0467e9afff2e1e4b0b185e2e25d3
Mikhail Kasimov <nsmikawork@gmail.com>
2024-04-26 20:48:57 +0300

    Format code

--- src/Randoms.java
+++ src/Randoms.java
@@ -14,8 +14,7 @@ public class Randoms implements Iterable<Integer> {
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>()
-        {
+        return new Iterator<Integer>() {
             @Override
             public boolean hasNext() {
                 return true;
