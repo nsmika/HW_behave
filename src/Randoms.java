00e2b187cba6746864948ffb5216e64ba7547ac8
Mikhail Kasimov <nsmikawork@gmail.com>
2024-04-26 20:48:07 +0300

    Initial project

---
+++ src/Randoms.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Random;
+
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min;
+    int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>()
+        {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return new Random().nextInt(min, max + 1);
+
+            }
+        };
+    }
+
+}
